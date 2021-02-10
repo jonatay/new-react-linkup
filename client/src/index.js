02f142b71f0c580153ac5615c6604e424b1c6c39
jonatay <jono.stretch@gmail.com>
2021-02-10 15:34:40 +0200

    redux starting and devtools working

--- client/src/index.js
+++ client/src/index.js
@@ -2,24 +2,32 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+// import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import configureStore, { history } from "./configureStore";
 import { ConnectedRouter } from "connected-react-router";
+import { initAuth } from "./mid/common/auth";
 const initialState = {};
 const store = configureStore(initialState);
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <ConnectedRouter history={history}>
-        <App />
-      </ConnectedRouter>
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+
+function render(Component) {
+  ReactDOM.render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <ConnectedRouter history={history}>
+          <Component />
+        </ConnectedRouter>
+      </Provider>
+    </React.StrictMode>,
+    document.getElementById("root")
+  );
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
+
+initAuth(store.dispatch)
+  .then(() => render(App))
+  .catch((error) => console.error(error));
